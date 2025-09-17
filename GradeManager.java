9ac7c6b0d34cba50d5f392c0271b7db258cf109b
Tomar Clark <163206387+tomarclark8@users.noreply.github.com>
2025-09-17 06:54:22 -0400

    Update GradeManager.java

--- GradeManager.java
+++ GradeManager.java
@@ -1,4 +1,6 @@
+import java.lang.Math;
 import java.util.ArrayList;
+import java.util.*;
 
 public class GradeManager{
   //instance variables
@@ -8,26 +10,39 @@ public class GradeManager{
   public GradeManager(){
     this.classList = new ArrayList<Student>();
   }
+  //displays students in class list
+  public String classListToString(){
+    String classListString = "";
+    for(int i = 0; i < classList.size(); i++){
+      classListString += "Student ID: "+classList.get(i).getStudentId() + "\n";
+    }
+    return classListString;
+  }
   //add student to array
   public void addStudent(Student student){
       classList.add(student);
       System.out.println("Student Added Successfully");
       
   }
+  //find student by id
 
+  //cant make void (<--- fix this))
   public Student findStudent(int studentId){
     //find student by id
     if(classList.isEmpty()){
       System.out.println("Class List is Empty");
+      return null;
     }
     for(int i = 0; i < classList.size(); i++){
       if(classList.get(i).getStudentId() == studentId){
         return classList.get(i);
       }
     }
+    // Student not found after checking all students
+    System.out.println("Student Not Found");
+    System.out.println("The only students in the class are: \n" + classListToString());
     return null;
   }
-    
   //get average of all test scores
   public double getAverageTestScore(){
     double sum = 0;
